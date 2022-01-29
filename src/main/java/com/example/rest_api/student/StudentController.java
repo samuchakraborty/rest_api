d8ae3049a95a4a7933a871f50aa31fbe9e05a876
Samu Chakraborty <61682653+samuchakraborty@users.noreply.github.com>
2022-01-29 21:47:35 +0600

    update data by jpa

--- src/main/java/com/example/rest_api/student/StudentController.java
+++ src/main/java/com/example/rest_api/student/StudentController.java
@@ -3,7 +3,6 @@ package com.example.rest_api.student;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-
 import java.util.List;
 
 @RestController
@@ -24,13 +23,26 @@ public class StudentController {
     }
 
     @PostMapping
-    public void  registerNewStudent(@RequestBody Student student){
+    public void registerNewStudent(@RequestBody Student student) {
         studentService.addNewStudent(student);
     }
+
     @DeleteMapping(path = "{studentId}")
-    private  void deleteStudent(@PathVariable("studentId") Long id){
+    public void deleteStudent(@PathVariable("studentId") Long id) {
         studentService.deleteStudent(id);
 
     }
+
+    @PutMapping(path = "{studentId}")
+    public void updateStudent(
+            @PathVariable("studentId") Long studentId,
+            @RequestParam(required = false) String name,
+
+            @RequestParam(required = false) String email
+    ) {
+        studentService.updateStudent(studentId, name, email);
+
+    }
+
 
 }
